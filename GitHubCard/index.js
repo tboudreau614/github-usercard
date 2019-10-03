1d5da79c0657e5b2295ee9c7a4466264c271152b
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-03 14:50:55 -0600

    Got all cards to show except my own

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,21 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
+
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+    .then(response =>{
+      const newCards = cardBuild(response.data);
+      profile.appendChild(newCards);
+    })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +59,73 @@ const followersArray = [];
 </div>
 
 */
+
+axios.get('https://api.github.com/users/tboudreau614')
+.then(function (response) {
+  console.log(response);
+})
+.catch(function(error) {
+  console.log(error);
+})
+.finally(function(){
+});
+
+const profile = document.querySelector('.cards');
+
+//function start
+
+function cardBuild (arr) {
+    const divCard = document.createElement('div');
+    const userImg = document.createElement('img')
+    const cardInfo = document.createElement('div');
+    const cardName = document.createElement('h3');
+    const userName = document.createElement('p')
+    const userLocation = document.createElement('p');
+    const userProfile = document.createElement('p')
+    const pageLink = document.createElement('a');
+    const followers = document.createElement('p')
+    const following = document.createElement('p');
+    const userBio = document.createElement('p')
+
+    //appends (elem attach)
+
+    profile.appendChild(divCard);
+    divCard.appendChild(userImg);
+    divCard.appendChild(cardInfo);
+    cardInfo.appendChild(cardName);
+    cardInfo.appendChild(userName);
+    cardInfo.appendChild(userLocation);
+    cardInfo.appendChild(userProfile);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(userBio);
+    userProfile.appendChild(pageLink);
+
+    //classLists
+
+    divCard.classList.add('card');
+    cardInfo.classList.add('card-info');
+    cardName.classList.add('name');
+    userName.classList.add('username');
+
+    //content
+
+    userImg.src = arr.avatar_url;
+    cardName.textContent = arr.name;
+    userName.textContent = arr.login;
+    userLocation.textContent = `Location: ${arr.location}`;
+    userProfile.textContent = `Profile: `;
+    pageLink.href = arr.html_url;
+    userProfile.textContent = `Profile: ${pageLink.href}`;
+    followers.textContent = `Followers: ${arr.followers}`;
+    following.textContent = `Following: ${arr.following}`;
+    userBio.textContent = `Bio: ${arr.bio}`;
+
+  return divCard
+
+}
+
+console.log(cardBuild);
 
 /* List of LS Instructors Github username's: 
   tetondan
